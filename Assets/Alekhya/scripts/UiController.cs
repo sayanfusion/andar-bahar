385fd89f16904449e647aa40d81e82b71b2e8075
AlekhyaGameDev <fusiontechlab386@gmail.com>
2025-08-07 13:05:13 +0530

    Updated

--- Assets/Alekhya/scripts/UiController.cs
+++ Assets/Alekhya/scripts/UiController.cs
@@ -42,7 +42,7 @@ public class UiController : MonoBehaviour
     }
 
 
-    public void BackToLobby()
+    public void BackToMainMenu()
     {
         SceneManager.LoadScene("PlayerMainMenu");
     }
